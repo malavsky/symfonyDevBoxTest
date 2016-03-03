30859c2964f004a82e9213a90f514fd2fbf12c81
Alexey Chernoivan <achernoivan@grossum.com>
2016-03-03 16:28:38 +0200

    - make working with last StaticPageBundle version

---
+++ htdocs/src/Application/Grossum/StaticPageBundle/Entity/Repository/StaticPageRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Application\Grossum\StaticPageBundle\Entity\Repository;
+
+use Grossum\StaticPageBundle\Entity\Repository\BaseStaticPageRepository;
+
+class StaticPageRepository extends BaseStaticPageRepository
+{
+
+}
